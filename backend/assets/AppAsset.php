6559f6baf37ec8ec19e7f64b352d7a13b1a07e1f
Pankaj Sanam <pankaj.sanam@gmail.com>
2017-05-20 05:13:35 +0000

    Added assets for backend

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -9,15 +9,37 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
+    
     public $css = [
-        'css/site.css',
+        'css/AdminLTE.min.css',
     ];
+    
     public $js = [
+        'js/app.min.js'
     ];
+    
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
+    
+    /**
+     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
+     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
+     */
+    public $skin = '_all-skins';
+    
+    public function init()
+    {
+        // Append skin color file if specified
+        if ($this->skin) {
+            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
+                throw new Exception('Invalid skin specified');
+            }
+            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
+        }
+        parent::init();
+    }
 }
