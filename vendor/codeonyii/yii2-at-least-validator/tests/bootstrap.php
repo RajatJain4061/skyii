d9c07e410c95053824efd9c75688b3f30ce05d6d
Pankaj Sanam <pankaj.sanam@gmail.com>
2017-06-23 10:29:25 +0000

    Removed vendor folder

--- vendor/codeonyii/yii2-at-least-validator/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$vendor = __DIR__ . '/../vendor';
-require_once ($vendor . '/autoload.php');
-require_once ($vendor . '/yiisoft/yii2/Yii.php');
--- vendor/kdn/yii2-domain-validator/tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// ensure we get report on all possible php errors
-error_reporting(-1);
-
-$vendorPath = dirname(__DIR__) . '/vendor';
-require_once("$vendorPath/autoload.php");
-require_once("$vendorPath/yiisoft/yii2/Yii.php");
